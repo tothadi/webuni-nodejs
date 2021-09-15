93deef05c9427d0339d69b285cd05bd8a2d0030f
tothadi <tothadam.onp@gmail.com>
2021-09-15 17:37:55 +0200

    corrected req.params.uid

--- greeter/middlewares/user/getUserByID_MW.js
+++ greeter/middlewares/user/getUserByID_MW.js
@@ -1,6 +1,6 @@
 /**
  * Finds user based on req.params.uid or session.uid
- * 
+ *
  * 1. Checks if req.params.uid or session.uid exists if not return next
  * 2. if req.params.uid define ID as req.param.uid
  * 3. if no req.params.uid define ID as session.uid
@@ -13,12 +13,18 @@
 module.exports = (objRep) => {
 	const { userModel } = objRep;
 	return (req, res, next) => {
-		if (typeof req.session.uid === 'undefined' && typeof req.params.uid === 'undefined') {
+		if (
+			typeof req.session.uid === 'undefined' &&
+			typeof req.params.uid === 'undefined'
+		) {
 			return next();
 		}
 
-        const id = req.params.uid.split('.')[0] || req.session.uid;
-		
+		if (typeof req.params.uid !== 'undefined' && req.params.uid.includes('.')) {
+			req.params.uid = req.params.uid.split('.')[0];
+		}
+		const id = req.params.uid || req.session.uid;
+
 		try {
 			res.locals.user = userModel.findOne({ uid: id });
 		} catch (err) {
